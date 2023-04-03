d736748b5c87b53dee61c2934ff84a429192045f
Ravi Teja Gudapati <tejainece@gmail.com>
2023-04-03 06:22:26 +0200

    Circle tip

--- bin/main.dart
+++ bin/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
               width: 200,
               height: 100,
               color: Colors.grey,
-              child: CustomPaint(
+              child: const CustomPaint(
                   painter: ConnectionsPainter(connections: [
                 Connection(start: Offset(0, 0), end: Offset(200, 100)),
                 Connection(start: Offset(0, 50), end: Offset(200, 50)),
@@ -32,13 +32,22 @@ class MyApp extends StatelessWidget {
               ])),
             ),
             Row(children: [
-              make(0.1),
-              make(0.3),
-              make(0.5),
-              make(0.7),
-              make(0.9),
-              make(1),
-              make(1.2),
+              make(0.1, tip: ArrowTip()),
+              make(0.3, tip: ArrowTip()),
+              make(0.5, tip: ArrowTip()),
+              make(0.7, tip: ArrowTip()),
+              make(0.9, tip: ArrowTip()),
+              make(1, tip: ArrowTip()),
+              make(1.2, tip: ArrowTip()),
+            ]),
+            Row(children: [
+              make(0.1, tip: CircleTip()),
+              make(0.3, tip: CircleTip()),
+              make(0.5, tip: CircleTip()),
+              make(0.7, tip: CircleTip()),
+              make(0.9, tip: CircleTip()),
+              make(1, tip: CircleTip()),
+              make(1.2, tip: CircleTip()),
             ]),
           ],
         ),
@@ -47,19 +56,37 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Widget make(double bend) {
+Widget make(double bend, {TipMaker? tip}) {
   return Container(
     width: 200,
     height: 400,
+    margin: EdgeInsets.only(right: 20),
     color: Colors.black12,
     child: CustomPaint(
       painter: ConnectionsPainter(
         connections: [
-          Connection(start: Offset(0, 200), end: Offset(200, 0), bend: bend),
-          Connection(start: Offset(0, 200), end: Offset(200, 100), bend: bend),
-          Connection(start: Offset(0, 200), end: Offset(200, 200), bend: bend),
-          Connection(start: Offset(0, 200), end: Offset(200, 300), bend: bend),
-          Connection(start: Offset(0, 200), end: Offset(200, 400), bend: bend),
+          Connection(
+              start: Offset(0, 200), end: Offset(200, 0), bend: bend, tip: tip),
+          Connection(
+              start: Offset(0, 200),
+              end: Offset(200, 100),
+              bend: bend,
+              tip: tip),
+          Connection(
+              start: Offset(0, 200),
+              end: Offset(200, 200),
+              bend: bend,
+              tip: tip),
+          Connection(
+              start: Offset(0, 200),
+              end: Offset(200, 300),
+              bend: bend,
+              tip: tip),
+          Connection(
+              start: Offset(0, 200),
+              end: Offset(200, 400),
+              bend: bend,
+              tip: tip),
         ],
       ),
     ),
--- lib/connection_arrow.dart
+++ lib/connection_arrow.dart
@@ -1,77 +1,143 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:arrow_path/arrow_path.dart';
 
-export 'arrow_painter.dart';
+class Connection {
+  final Offset start;
+  final Offset end;
+  final double bend;
+  final Paint? stroke;
+  final TipMaker? tip;
 
-/*
-class HorizontalArrow extends CustomPainter {
-  final ArrowConfig config;
+  const Connection(
+      {required this.start,
+      required this.end,
+      this.bend = 0.5,
+      this.stroke,
+      this.tip});
 
-  const HorizontalArrow({required this.config});
+  Path path() {
+    final width = (end.dx - start.dx).abs();
+    double endAnchorX = width * (1 - bend);
+    if (width - endAnchorX < 75) {
+      endAnchorX = 75;
+    }
+    return Path()
+      ..moveTo(start.dx, start.dy)
+      ..cubicTo(
+        width * bend,
+        start.dy,
+        endAnchorX,
+        end.dy,
+        end.dx,
+        end.dy,
+      );
+  }
+}
+
+class ConnectionsPainter extends CustomPainter {
+  final List<Connection> connections;
+
+  const ConnectionsPainter({this.connections = const []});
 
   @override
   void paint(Canvas canvas, Size size) {
-    final startPoint = config.startPoint(size);
-    final startAnchor = config.startAnchor(size);
-    final endPoint = config.endPoint(size);
-    final endAnchor = config.endAnchor(size);
-    Path bezierPath = Path()
-      ..moveTo(startPoint.dx, startPoint.dy)
-      ..cubicTo(
-        startAnchor.dx,
-        startAnchor.dy,
-        endAnchor.dx,
-        endAnchor.dy,
-        endPoint.dx,
-        endPoint.dy,
-      );
-    bezierPath = ArrowPath.make(path: bezierPath);
+    for (final connection in connections) {
+      final paint = connection.stroke ?? defaultPaint;
+      final path = connection.path();
+      canvas.drawPath(path, paint);
+    }
 
-    final bezierPaint = Paint()
-      ..color = Colors.black
-      ..strokeWidth = 2
-      ..style = PaintingStyle.stroke
-      ..strokeJoin = StrokeJoin.round;
+    for (final connection in connections) {
+      connection.tip?.paint(canvas, connection.end);
+    }
+  }
 
-    canvas.drawPath(bezierPath, bezierPaint);
+  static final defaultPaint = Paint()
+    ..color = Colors.black
+    ..strokeWidth = 2
+    ..style = PaintingStyle.stroke
+    ..strokeJoin = StrokeJoin.round;
+
+  @override
+  bool shouldRepaint(ConnectionsPainter oldDelegate) => true;
+}
+
+abstract class TipMaker {
+  Paint? get stroke;
+  Paint? get fill;
+
+  void paint(Canvas canvas, Offset point);
+}
+
+class ArrowTip implements TipMaker {
+  final double angle;
+  final double length;
+  @override
+  late final Paint? stroke;
+  @override
+  late final Paint? fill;
+
+  ArrowTip({this.angle = 30, this.length = 15, Paint? stroke, Paint? fill}) {
+    if (stroke == null && fill == null) {
+      this.stroke = Paint()
+        ..color = Colors.black
+        ..strokeWidth = 2
+        ..style = PaintingStyle.stroke
+        ..strokeJoin = StrokeJoin.round;
+      this.fill = null;
+    } else {
+      this.stroke = stroke;
+      this.fill = fill;
+    }
+  }
+
+  Path _path(Offset point) {
+    double angle = _toRadian(180 - this.angle);
+    final start = Offset(length * cos(angle), length * sin(angle));
+    final end = Offset(length * cos(-angle), length * sin(-angle));
+    return Path()
+      ..moveTo(point.dx, point.dy)
+      ..relativeLineTo(start.dx, start.dy)
+      ..moveTo(point.dx, point.dy)
+      ..relativeLineTo(end.dx, end.dy);
   }
 
   @override
-  bool shouldRepaint(HorizontalArrow oldDelegate) =>
-      oldDelegate.config.isEqual(config);
+  void paint(Canvas canvas, Offset point) {
+    if (stroke != null) canvas.drawPath(_path(point), stroke!);
+    if (fill != null) canvas.drawPath(_path(point), fill!);
+  }
 }
 
-class ArrowConfig {
-  final double bend;
-  final CurveDirection direction;
+class CircleTip implements TipMaker {
+  final double radius;
+  @override
+  late final Paint? stroke;
+  @override
+  late final Paint? fill;
 
-  ArrowConfig({this.bend = 0.5, this.direction = CurveDirection.topToBottom});
-
-  Offset startPoint(Size size) {
-    switch(direction) {
-      case CurveDirection.topToBottom:
-        return Offset(0, 0);
-      case CurveDirection.bottomToTop:
-        return Offset(0, size.height);
-      case CurveDirection.straight:
-        return Offset(0, size.height / 2);
+  CircleTip({this.radius = 5, Paint? stroke, Paint? fill}) {
+    if (stroke == null && fill == null) {
+      this.stroke = Paint()
+        ..color = Colors.black
+        ..strokeWidth = 2
+        ..style = PaintingStyle.stroke
+        ..strokeJoin = StrokeJoin.round;
+      this.fill = Paint()
+        ..color = Colors.white
+        ..style = PaintingStyle.fill;
+    } else {
+      this.stroke = stroke;
+      this.fill = fill;
     }
   }
-  Offset startAnchor(Size size) {
-    Offset(size.width * bend, topToBottom ? 0 : size.height)
+
+  @override
+  void paint(Canvas canvas, Offset point) {
+    if (stroke != null) canvas.drawCircle(point, radius, stroke!);
+    if (fill != null) canvas.drawCircle(point, radius, fill!);
   }
-  Offset endPoint(Size size) =>
-      Offset(size.width, topToBottom ? size.height : 0);
-  Offset endAnchor(Size size) =>
-      Offset(size.width * (1 - bend), topToBottom ? size.height : 0);
-
-  bool isEqual(ArrowConfig other) =>
-      bend == other.bend && topToBottom == other.topToBottom;
 }
 
-enum CurveDirection {
-  topToBottom,
-  straight,
-  bottomToTop,
-}
- */
+double _toRadian(double degree) => degree * pi / 180;
